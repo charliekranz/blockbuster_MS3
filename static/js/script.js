ce1153f7417f76577172e4281c450f26ac0f56fd
charliekranz <charliekranz3@gmail.com>
2021-05-12 20:15:05 +0000

    Cleaned-up css

--- static/js/script.js
+++ static/js/script.js
@@ -15,7 +15,6 @@ function clickme() {
 
 }
 
-
 /* check that at lease one checkbox is checked before submitting form */
 /* Ronan helped me by finding the e.prevent method and suggested naming form to avoid issue later */
 /* e.prevent found at ttps://stackoverflow.com/questions/35552813/call-function-with-bootstrap-submit-button */
@@ -30,20 +29,7 @@ $(".story-form").on('submit', function(e) {
 
 /* Limit Cast to 3 using code found here - http://jsfiddle.net/vVxM2/ and syntax tweaked with assistance of 
 Ronan to target cast-checkbox correctly and call modal */
-
-
-/*var min = 1;
-$('.cast-checkbox').on('change', function(evt) {
-   if($('.cast-checkbox:checked').length < min) {
-       $('#minModal').modal("show")
-       this.checked = false;
-   }
-});*/
-
-
 /* Using code and adapting to show modal if no cast chosen  https://www.sitepoint.com/jquery-check-checkbox-checked/ */
-
-
 
 var limit = 3;
 $('.cast-checkbox').on('change', function(evt) {
@@ -59,14 +45,4 @@ $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus');
 });
 
-/* $(function() {
-  $('#Alpha').on("submit", function() {
-    if ($('input[name=cast-checkbox]').attr('checked') != false) {
-	$('#minModal').modal("show");
-      return false;
-    } else {
-      submit;
-      return true;
-    }
-  });
-}); */
+
